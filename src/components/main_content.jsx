ef81e8c6a53bcb91b49b5f966a07d7a49e5e23e7
telare <andrewzhubinskiy21@gmail.com>
2024-10-22 21:06:38 +0300

    2.v

--- src/components/main_content.jsx
+++ src/components/main_content.jsx
@@ -1,49 +1,52 @@
 import { useRef, useState } from "react";
-import MathJax from "react-mathjax2";  
+
 
 
 function Main() {
     const inputRef = useRef('')
-    //const resultRef = useRef('')
-
+    const resultRef = useRef('')
     const alertRef = useRef('')
 
     const regexp = new RegExp(/[\\/###**]/, 'g');
-    const regexp2 = new RegExp(/([a-zA-Z])\1+/,'g')
-    const [formatResult, setFotmatResult] = useState('')
+    const regexp2 = new RegExp(/([a-zA-Z])\1+/, 'g')
+
+    
     const [currentTheme, setTheme] = useState('To dark theme')
     const [currentBg, setBg] = useState('bg-slate-200')
     const [currentBtnBg, setBtnBg] = useState('bg-black')
     const [currentFontBtnBg, setFontBtnBg] = useState('text-white')
+
+    const [result, setResult] = useState('')
+    const [copyAlert, setCopyAlert] = useState('')
    
     function input_edit() {
-        if(inputRef.current.value.match(regexp) !== null ){
-            
-            setFotmatResult(`${inputRef.current.value}`)
-            
-            //resultRef.current.value = inputRef.current.value.replace(regexp,'')
-            //resultRef.current.value = `\\\(${inputRef.current.value})\\`    
-        }
-        // if (inputRef.current.value.match(regexp2) !== null){
-        //     //resultRef.current.value = inputRef.current.value.replace(regexp2,'$1')
-        // }
+        let inputValue = inputRef.current.value
+        if(inputValue.match(regexp) || inputValue.match(regexp2)!== null  ){
+            inputValue = inputValue.replace(regexp, '')
+            inputValue = inputValue.replace(regexp2, '')
+            setResult(inputValue)
+        }else setResult(inputValue)
+        
         
     }
 
     // change this to setTimeOut
 
     function copy(){
-        if(inputRef.current.value !=0 && resultRef.current.value != 0){
+        if(inputRef.current.value != undefined && resultRef.current.value != undefined){
             navigator.clipboard.writeText(resultRef.current.value);
-        
-            alertRef.current.innerText = 'Copied!'
+            setCopyAlert('Copied!')
+               setTimeout(() => {
+                        setCopyAlert('')
+                }, 3000);
+            
         }
     } 
     
     
     function input_reset() {
         inputRef.current.value = '';
-        resultRef.current.value = '';
+        setResult('')
     }
 
 
@@ -65,19 +68,14 @@ function Main() {
             <div className="flex justify-center">
 
                 <div className="flex self-center">
-                    <input type="text" name="" id="user_input" placeholder="Paste here" 
-                    className="mr-4 rounded-xl border-black  border-2 h-80 w-96
-                     placeholder:top-1 placeholder:left-1  placeholder:absolute   autofocus" ref={inputRef}/>
+                    <textarea id="result" rows="5" cols="33" className="rounded-xl border-black border-2 h-80 w-96 mr-10" ref={inputRef}>
+                    </textarea>
                 </div>
 
                 <div className="flex self-center">
-                <div className="rounded-xl border-black border-2 h-80 w-96" >
-                        <MathJax.Context>
-                            <div>
-                                <MathJax.Node inline>{formatResult}</MathJax.Node> 
-                            </div>
-                        </MathJax.Context>
-                    </div>
+                    <textarea id="result" rows="5" cols="33" className="rounded-xl border-black border-2 h-80 w-96" value={result}>
+                    </textarea>
+                 
                 </div>
 
             </div>
@@ -86,7 +84,9 @@ function Main() {
                 <button className={`rounded-md border ${currentBtnBg} ${currentFontBtnBg} mr-3 w-20`} onClick={copy} >Copy</button>
                 <button className={`rounded-md border-2 ${currentBtnBg} ${currentFontBtnBg} w-20`} onClick={input_reset}>Reset</button>
             </div>
-            <div className='w-12 h-5  self-center' ref={alertRef}></div>
+            <div className='w-12 h-5  self-center'>
+                <div>{copyAlert}</div>
+            </div>
     </div>
   )
     
